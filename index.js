6f55d0ee1826af623a58a4136d6a6d99c7cd7442
jeeberharter1 <88991491+jeeberharter1@users.noreply.github.com>
2024-05-07 18:22:17 -0500

    Update index.js
    added comments to script file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// utility functions
+
+// open side navigation by adjusting width and darken background
 function openNav() {
   document.getElementById("side-nav").style.width = "250px";
   document.getElementById('bg').classList.add('bg-on');
   document.getElementById('bg').classList.remove('bg-off');
 }
 
+// close side nav by adjusting width and remove background darkness
 function closeNav() {
   document.getElementById("side-nav").style.width = "0";
   document.getElementById('bg').classList.add('bg-off');
